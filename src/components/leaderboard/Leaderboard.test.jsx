3a9f9b45f744b5547a92c1a0d5c2def1df7adc8f
Josh Wright <joshua.wright@ada.ac.uk>
2026-03-23 10:39:56 +0000

    Fix ESLint: remove unused render imports in GameOver and Leaderboard tests

--- src/components/leaderboard/Leaderboard.test.jsx
+++ src/components/leaderboard/Leaderboard.test.jsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import Leaderboard from './Leaderboard';
 import { renderWithRouter } from '../../test/helpers/renderHelpers';
 
